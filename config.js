d6b086ed99d360326631e3d19e5a6cf0ba94cb1b
Asif Sheikh <116940358+notasifoffficial@users.noreply.github.com>
2024-01-26 11:39:16 +0600

    config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  status: 'Hasai Sasaki',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -13,7 +13,7 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+    url: "https://www.youtube.com/@GW__Maruf",
   },
 
   voteManager: {
