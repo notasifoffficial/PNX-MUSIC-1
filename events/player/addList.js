48df0773f511d8b08d5b6057c00122047eea5286
Asif Sheikh <116940358+notasifoffficial@users.noreply.github.com>
2024-01-26 12:19:48 +0600

    addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -5,17 +5,5 @@ queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} 
 }
 
 /*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+Made by notasifofficial
 */
